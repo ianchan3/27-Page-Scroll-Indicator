33c12940ea27617bda96c22ff7c1687836260a0c
ianchan3 <ianchan3138@gmail.com>
2023-02-21 12:25:29 -0800

    Day 27 of 100 Days of JavaScript Completed

--- js/app.js
+++ js/app.js
@@ -16,3 +16,19 @@ window.addEventListener("scroll", () => {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+window.onscroll = () => scrollProgress();
+
+function scrollProgress() {
+  const currentState = document.body.scrollTop || document.documentElement.scrollTop;
+
+  const pageHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+  const scrollPercentage = (currentState / pageHeight ) * 100;
+
+  const progressBar = document.querySelector(".progress");
+
+  progressBar.style.visibility = "visible";
+  progressBar.style.width = scrollPercentage + "%";
+}
+
